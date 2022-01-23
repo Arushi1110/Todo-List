3a7b98f36cf2d414bd8301b8f76664f42d6572e1
Arushi1110 <arushigupta568@gmail.com>
2022-01-24 00:26:21 +0530

    todo structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+// import components
+import Form from './components/Form.js'
+import TodoList from './components/TodoList.js';
+
 function App() {
+  const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState("all")
+  const [filteredTodos, setFilteredTodos] = useState([])
+
+  useEffect(()=> {filterHandler()},[todos,status])
+
+  const filterHandler=()=>{
+    switch(status){
+      case "completed" :
+        setFilteredTodos(todos.filter((todo)=>todo.completed === true))
+        break;
+      case "uncompleted" :
+        setFilteredTodos(todos.filter((todo)=>todo.completed === false))
+        break;
+      default:
+        setFilteredTodos(todos)
+        break;
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Hello there mate.</h1>
+      <header>Focus On Your Day </header>
+
+      <Form 
+        inputText = {inputText} 
+        setInputText = {setInputText} 
+        todos = {todos} 
+        setTodos = {setTodos}
+        setStatus = {setStatus}/>
+
+      <TodoList todos = {todos} 
+        setTodos = {setTodos}
+        filteredTodos={filteredTodos}
+        />
     </div>
   );
-}
+} 
 
 export default App;
